0848a791a381214c4f9d58ff4fe14a6a27372b5b
Tyuiu.milyutin@outlook.com <mm1llnikita@gmail.com>
2024-10-30 09:46:47 +0500

    Task7

---
+++ Tyuiu.MilyutinND.Sprint3.Task7.V7/Program.cs
@@ -0,0 +1,39 @@
+﻿using Tyuiu.MilyutinND.Sprint3.Task7.V7.Lib;
+
+DataService ds = new DataService();
+
+Console.Title = "Спринт #3 | Выполнил: Милютин Н. Д. | СМАРТб-24-1";
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* Спринт #3                                                               *");
+Console.WriteLine("* Тема: Добавление к решению итоговых проектов по спринту                 *");
+Console.WriteLine("* Задание #7                                                              *");
+Console.WriteLine("* Вариант #7                                                              *");
+Console.WriteLine("* Выполнил: Милютин Никита Дмитриевич | СМАРТб-24-1                       *");
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* УСЛОВИЕ:                                                                *");
+Console.WriteLine("* Написать программу, которая выводит таблицу значений функции            *");
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+Console.WriteLine("***************************************************************************");
+
+int startValue = -5;
+int stopValue = 5;
+Console.WriteLine("Старт шага = " + startValue);
+Console.WriteLine("Конец шага = " + stopValue);
+
+int len = ds.GetMassFunction(startValue, stopValue).Length;
+double[] res = new double[len];
+res = ds.GetMassFunction(startValue, stopValue);
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+Console.WriteLine("***************************************************************************");
+
+Console.WriteLine("+----------+----------+");
+Console.WriteLine("|     X    |   f(x)   |");
+Console.WriteLine("+----------+----------+");
+for (int i = 0; i <= len - 1; i++)
+{
+    Console.WriteLine("|{0,5:d}     | {1,6:f2}   |", startValue, res[i]);
+    startValue++;
+}
+Console.WriteLine("+----------+----------+");
